6b34f7d02edb7280f3337ed5fddafcd872a7bdce
Raunak Roy <raunakroy090@gmail.com>
2026-07-26 01:29:08 +0530

    Time: 22 ms (62.83%), Space: 13.2 MB (46.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,14 +8,14 @@ class Solution(object):
         sett=set()
         n=len(s)
         longest=0
-        count=0
+        
         for r in range(n):
             
             while s[r] in sett:
                 sett.remove(s[l])
                 l+=1
-                count-=1
-            sett.add(s[r])
-            count+=1
+            count=(r-l)+1
             longest=max(longest,count)
+            sett.add(s[r])
+
         return longest
