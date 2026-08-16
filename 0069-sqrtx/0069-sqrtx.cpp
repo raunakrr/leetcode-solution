ceb06b9ab46168af35ab597c8a2a5ac0f8093819
Raunak Roy <raunakroy090@gmail.com>
2026-08-17 00:56:41 +0530

    Time: 0 ms (100%), Space: 8.4 MB (87.8%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int start=1;
+        int end = x;
+        int mid;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(mid>x/mid) end=mid-1;
+            else if(mid<x/mid) start=mid+1;
+            else return mid;
+        }
+        return end;
+    }
+};
