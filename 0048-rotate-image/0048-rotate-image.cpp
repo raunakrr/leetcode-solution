4997e7dea09ea231f7ed183fac7c4b5399e90971
Raunak Roy <raunakroy090@gmail.com>
2026-09-08 15:06:11 +0530

    Time: 0 ms (100%), Space: 10.4 MB (9.09%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int maxr=matrix.size();
+        int maxc=matrix[0].size();
+        int minr=0,minc=0;
+        int tne=maxc*maxr;
+        int count=0;
+        for(int i=0;i<maxr;i++){
+            for(int j=i;j<maxc;j++){ //or j=0;j<i
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;   
+            }
+        }
+        for(int i=0;i<maxr;i++){
+            for(int j=0,k=maxr-1;j<k;j++,k--){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][k];
+                matrix[i][k]=temp;
+            }
+        }
+
+    }
+};
